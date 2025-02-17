61cae16d4fad911987b3366dca3c0e47d4e9c900
Marcelo <marceloalves87@gmail.com>
2025-02-17 13:44:28 -0300

    Cleaning Repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TvComponent } from './tv/tv.component';
 import { DatePipe } from '@angular/common'
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    TvComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [DatePipe],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        TvComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [DatePipe, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
